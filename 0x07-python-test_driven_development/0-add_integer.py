c92fcff1b3019f1f17d36f8f34ad2d14df58df0a
Leukeng <ekengeffiong21@gmail.com>
2023-12-09 12:29:41 -0800

    task done

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,15 +1,21 @@
 #!/usr/bin/python3
-add_integer = __import__('0-add_integer').add_integer
+"""
+This module has one function that adds up 2 integers
+"""
 
-print(add_integer(1, 2))
-print(add_integer(100, -2))
-print(add_integer(2))
-print(add_integer(100.3, -2))
-try:
-    print(add_integer(4, "School"))
-except Exception as e:
-    print(e)
-try:
-    print(add_integer(None))
-except Exception as e:
-    print(e)
+
+def add_integer(a, b=98):
+    """Return the sum of two integers or floats as integers
+    Args:
+        a: first argument
+        b: second argument
+    Returns:
+        Sum of the two arguments
+    Raises:
+        TypeError: If either of the arguments not an integer or a float
+    """
+    if ((not isinstance(a, int) and not isinstance(a, float))):
+        raise TypeError("a must be an integer")
+    if ((not isinstance(b, int) and not isinstance(b, float))):
+        raise TypeError("b must be an integer")
+    return (int(a) + int(b))
